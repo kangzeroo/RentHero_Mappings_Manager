ad4edcdd85210078c28f395f8b7887cf6bdec5a4
Jimmy <j63guo@uwaterloo.ca>
2018-05-07 12:41:47 -0400

    asd

--- api/knowledge_domains/js/specific_struc_intents.js
+++ api/knowledge_domains/js/specific_struc_intents.js
@@ -1,10 +1,37 @@
 exports.intents = {
   domain_prefix: '.SPEC_STRUC',
   relationships: [
+
     {
-      dialogFlow_intentName: '.SPEC_STRUC---Liability',
-      dialogFlow_intentID: 'projects/dev-landlordai-8221e/agent/intents/38ff2587-ecac-4102-b64d-3ffd48add5ed',
-      endpoint: 'YO MOMMA ENDPOINT'
-    }
+      dialogFlow_intentName: '.SPEC_STRUC---MonthlyCostPerRoom',
+      dialogFlow_intentID: 'projects/dev-landlordai/agent/intents/0211ae29-60cc-4b73-8776-299d56a12381',
+      endpoint: 'https://285f9b5c.ngrok.io/monthly_cost_per_room'
+    },
+    {
+      dialogFlow_intentName: '.SPEC_STRUC---Images',
+      dialogFlow_intentID: 'projects/dev-landlordai-8221e/agent/intents/ebc0403a-3a12-46bf-b55a-335ee0454d46',
+      endpoint: 'https://285f9b5c.ngrok.io/get_images_for_ad'
+    },
+    // get intent IDs
+    {
+      dialogFlow_intentName: '.SPEC_STRUC---Address',
+      dialogFlow_intentID: 'projects/dev-landlordai-8221e/agent/intents/ebc0403a-3a12-46bf-b55a-335ee0454d46',
+      endpoint: 'https://285f9b5c.ngrok.io/get_ad_address'
+    },
+    {
+      dialogFlow_intentName: '.SPEC_STRUC---PostalCode',
+      dialogFlow_intentID: 'projects/dev-landlordai-8221e/agent/intents/ebc0403a-3a12-46bf-b55a-335ee0454d46',
+      endpoint: 'https://285f9b5c.ngrok.io/get_ad_postal_code'
+    },
+    {
+      dialogFlow_intentName: '.SPEC_STRUC---Description',
+      dialogFlow_intentID: 'projects/dev-landlordai-8221e/agent/intents/ebc0403a-3a12-46bf-b55a-335ee0454d46',
+      endpoint: 'https://285f9b5c.ngrok.io/get_ad_description'
+    },
+    {
+      dialogFlow_intentName: '.SPEC_STRUC---Amenities',
+      dialogFlow_intentID: 'projects/dev-landlordai-8221e/agent/intents/ebc0403a-3a12-46bf-b55a-335ee0454d46',
+      endpoint: 'https://285f9b5c.ngrok.io/get_ad_amenities'
+    },
   ]
 }
