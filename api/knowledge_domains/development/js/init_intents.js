b42c672c75fe62cf8ccaf476599472c9064ddca4
Kangze Huang <kangzeroo@gmail.com>
2018-05-15 00:36:15 -0400

    init intents added

--- api/knowledge_domains/development/js/init_intents.js
+++ api/knowledge_domains/development/js/init_intents.js
@@ -4,7 +4,7 @@ exports.intents = {
     {
       dialogFlow_intentName: 'INIT---LandlordWidget',
       dialogFlow_intentID: 'projects/dev-landlordai-8221e/agent/intents/42b315ed-7eaa-41ba-9b1c-2d501e0c0b4a',
-      endpoint: 'https://f2b35613.ngrok.io/init_message'
+      endpoint: 'https://e7d213f0.ngrok.io/init_message'
     }
   ]
 }
